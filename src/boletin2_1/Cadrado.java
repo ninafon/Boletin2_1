da4985e900b478ddcca69e99294efc4371efddcb
Usuario <Usuario@Usuario-VAIO>
2021-04-27 19:47:39 +0200

    completamos clase Cadrado

--- src/boletin2_1/Cadrado.java
+++ src/boletin2_1/Cadrado.java
@@ -5,5 +5,23 @@ package boletin2_1;
 public class Cadrado {
     
     private float lado ;
+
+    public Cadrado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cadrado{" + "lado=" + lado + '}';
+    }
+    
     
 }
